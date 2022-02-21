bdb5a67494fa628edfb048dbd9f26e14e018f2d4
tomgause <71714664+tomgause@users.noreply.github.com>
2022-02-21 16:33:30 -0500

    done

--- data_testing.R
+++ data_testing.R
@@ -5,12 +5,9 @@
 library(tidyverse)
 library(lubridate)
 library(sf)
-
+library(hyfo)
 filename <- file.choose()
 data <- readRDS(filename)
-
-#Evil!!!
-#View(data)
 
 View(data %>% head(100))
 
@@ -23,10 +20,13 @@ data %>%
             min = min(obs_tmp_k),
             max = max(obs_tmp_k))
 
-#broken
 tom.data <- data %>%
-  mutate(time = as_datetime(forecast_target,
-                            format = "%Y%m"))
+  filter(forecast_target == 198301)
+print(length(tom.data$forecast_target))
+
+tom.data %>%
+  if
+  mutate(date = as_date(c(forecast_target, "01"), format="%Y%m%d"))
 View(tom.data %>% head(100))
 
 #adjust forecast_target to get observed averages for different months
@@ -39,25 +39,36 @@ tom.data %>%
                  y = y,
                  color = meanTemp))
 
+#find forecast means
 tom.data %>%
+  filter(forecast_target == 198301) %>%
   group_by(x, y) %>%
-  summarize(meanTemp = mean(obs_tmp_k)) %>%
+  summarize(mean_obs_Temp = mean(obs_tmp_k),
+    mean_pred_Temp = mean(fcst_tmp_k),
+    mse_Temp = mean(mean_obs_Temp - mean_pred_Temp)^2) %>%
   ggplot() +
   geom_point(aes(x = x,
                  y = y,
-                 color = meanTemp))
+                 color = mse_Temp))
 
-tom.data %>%
-  filter(time < as_datetime("2000-12-03")) %>%
-  group_by(x, y) %>%
-  summarize(meanTemp = mean(obs_tmp_k),
-            sdTemp = sd(obs_tmp_k)) %>%
-  arrange(sdTemp)
+corrected.data <- NULL
+corrected.data <- as.data.frame(corrected.data)
+  
+biasCorrect(
+  
+  #frc
+  tom.data %>%
+    filter(forecast_target == 198301) %>%
+    group_by(x, y) %>%
+    summarize(mean_obs_Temp = mean(obs_tmp_k),
+              mean_pred_Temp = mean(fcst_tmp_k)) %>%
+  mean_pred_Temp,
+  
+  #obs
+  mean_obs_Temp,
+  
+  #method
+  scaling
+)
 
-tom.data %>%
-  filter(x == -95.25, 
-         y == 49.50) %>%
-  summarize(meanTemp = mean(obs_tmp_k),
-            sd = sd(obs_tmp_k),
-            min = min(obs_tmp_k),
-            max = max(obs_tmp_k))
+
