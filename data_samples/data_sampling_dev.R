457375593168da7a90a0bf7d42ead1e0473077a0
tomgause <71714664+tomgause@users.noreply.github.com>
2022-03-07 21:43:31 -0500

    Made make_data, it works maybe???

--- data_samples/data_sampling_dev.R
+++ data_samples/data_sampling_dev.R
@@ -5,8 +5,12 @@ df <- list.files(path = "/Users/tomgause/Desktop/iScience_data/hindcasts_usa/",
                       full.names = TRUE)
 
 #generates 5 random samples from each month and lead time
-sample_size <- 5
-N <- length(df)
+#sample_size <- 5
+#N <- length(df)
+
+#first 9 lead times
+N <- 9
+
 index <- NULL
 temp <- NULL
 
@@ -15,14 +19,26 @@ hindcast_sample_100_from_each <- NULL
 hindcast_sample_1_cell_1_forecast <- NULL
 hindcast_sample_1_cell_all_forecaasts <- NULL
 hindcast_sample_100_cells_all_forecasts <- NULL
-hindcast_sample_all_cells_1_forecast <- NULL
+hindcast_sample_all_cells_1_month_1_lead <- NULL
+hindcast_sample_all_cells_1_month_9_leads <- NULL
+
+#"target_month", "x", "y", "fcst_cell", "target_month",
+#"lead", "fcst_tmp_k", "obs_tmp_k", "forecast_timestamp"
 
 for(i in 1:N) {
   print(i)
-  temp <- readRDS(df[i]) %>%
-    select("target_month", "x", "y", "cell", "target_month",
-           "lead", "fcst_tmp_k", "obs_tmp_k")
-
+  temp <- readRDS(df[1]) %>% #14056
+    filter(target_month == 1,
+           as.integer(forecast_timestamp) %% 1000000 == 120200)
+  
+  
+  test <- temp[index,]
+  
+  if (i == 1) {
+    hindcast_sample_all_cells_1_month_1_lead <- temp_all
+  }
+  
+  
   index1 <- sample(nrow(temp), 5, replace=FALSE)
   index2 <- sample(nrow(temp), 100, replace=FALSE)
   
@@ -36,4 +52,11 @@ for(i in 1:N) {
   
 }
 view(hindcast_data_set)
-saveRDS(hindcast_sample_5_from_each, file = "hindcast_sample_5_from_each.rds")
+#saveRDS(hindcast_sample_5_from_each, file = "sample_5_from_each.rds")
+#saveRDS(hindcast_sample_100_from_each, file="sample_100_from_each.rds")
+saveRDS(hindcast_sample_all_cells_1_month_1_lead, file="sample_all_cells_1_month_1_lead")
+saveRDS(hindcast_sample_all_cells_1_month_9_leads, file="sample_all_cells_1_month_9_leads")
+
+
+
+
---
+++ make_data.R
@@ -0,0 +1,54 @@
+library(tidyverse)
+library(lubridate)
+library(zoo)
+library(Dict)
+library(dplyr)
+
+
+df <- list.files(path = "/Users/tomgause/Desktop/iScience_data/hindcasts_usa/",
+                 pattern = ".rds",
+                 full.names = TRUE)
+
+#concat all data
+hindcast_all <- NULL
+N <- length(df)
+for(i in 1:N) {
+  sprintf("Reading %d/%d", i, N)
+  hindcast_all <- rbind(hindcast_all, readRDS(df[2]) %>%
+    mutate(lag1 = substring(forecast_timestamp, 1, 6)))
+}
+
+#temporal ordering by forecast timestamp (lag1 for more efficiency)
+hindcast_all <- hindcast_all[order(hindcast_all$lag1,)]
+
+#make dictionary from %Y%m to temperature (for efficiency)
+map_tt <- NULL
+map_tt <- dict(
+  "sample" = "test", #do this too keep dplyr from breaking Dict library :(
+  .class = "any",
+  .overwrite = FALSE
+)
+#generate mappings
+for (x in unique(hindcast_all$lag1)) {
+  map_tt["198301"] <- hindcast_all[match("198301", hindcast_all$forecast_target, nomatch=-1),]$obs_tmp_k
+}
+
+#make the lags with dplyr. Turns out forecast_timestamp was useful after all!
+hindcast_all <- hindcast_all %>%
+  mutate(lag1 = c(rep(map_tt["198301"], nrow(hindcast_all))),
+         #for each extra lag, we need to shift back 1 month.
+         #monthly, there are unique 14056 cells, 24 forecasts, and 9 lead times,
+         #so each lag needs to be 14056 * 24 * 9 = 3036096 rows
+         lag2 = lag(lag1, 3036096 * 1),
+         lag3 = lag(lag1, 3036096 * 2),
+         lag4 = lag(lag1, 3036096 * 3),
+         lag5 = lag(lag1, 3036096 * 4),
+         lag6 = lag(lag1, 3036096 * 5),
+         lag7 = lag(lag1, 3036096 * 6),
+         lag8 = lag(lag1, 3036096 * 7),
+         lag9 = lag(lag1, 3036096 * 8),
+         lag10 = lag(lag1, 3036096 * 9),
+         lag11 = lag(lag1, 3036096 * 10))
+
+view(head(hindcast_all, 100))
+view(tail(hindcast_all, 100))
