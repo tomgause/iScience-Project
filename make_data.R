2c4f13ee8e8587d5e1e7d36c4b94e1970cd53dff
acadiajean <ahegedus@middlebury.edu>
2022-03-30 15:16:25 -0400

    Fixing errors while runnning code, trying to add elevation data

--- make_data.R
+++ make_data.R
@@ -1,12 +1,12 @@
 ### make_data.R
-# Tom Gause
-# last edited 3/9/22
+# Tom Gause, Acadia Hegedus, and Katelyn Mei
+# last edited 3/29/22
 
 # system("cd data;
-#         curl https://wsim-datasets.s3.us-east-2.amazonaws.com/hindcasts_usa.tar;
-#         tar -xvf hindcasts_usa.tar;
-#         cd ..",
-#        intern = TRUE)
+#          curl https://wsim-datasets.s3.us-east-2.amazonaws.com/hindcasts_usa.tar;
+#          tar -xvf hindcasts_usa.tar;
+#          cd ..",
+#         intern = TRUE)
 
 library(tidyverse)
 library(lubridate)
@@ -15,7 +15,7 @@ library(dplyr)
 library(sf)
 
 #get paths to all data
-df <- list.files(path = "C:\\Users\\tgause\\iScience_Project\\data",
+df <- list.files(path = "C:\\Users\\ahegedus\\Documents\\iScience_Project\\data",
                  pattern = "hindcasts.*.rds",
                  full.names = TRUE)
 
@@ -37,7 +37,8 @@ hindcast_all <- dplyr::bind_rows(tmpL)
 #don't save yet as this will break R
 
 #choose 10% of pixels at random
-sample_data <- readRDS(file.choose()) #read in hindcast_month_1_lead_1.rds
+#read in hindcast_month_1_lead_1.rds
+sample_data <- readRDS(file = "C:\\Users\\ahegedus\\Documents\\iScience_Project\\data\\hindcasts_month_1_lead_1.rds") 
 
 all_pixels <- sample_data%>%
   dplyr::select(fcst_cell)%>%
@@ -86,8 +87,8 @@ hindcast_subset <- hindcast_subset%>%
          lag10 = find_lag_id(timestamp = hindcast_subset$forecast_timestamp, lag = 10),
          lag11 = find_lag_id(timestamp = hindcast_subset$forecast_timestamp, lag = 11),
          lag12 = find_lag_id(timestamp = hindcast_subset$forecast_timestamp, lag = 12))
-  #this works but gives a weird error?
-  
+#this works but gives a weird error?
+
 #Create observed data set (Lag Data)
 lag.data <- hindcast_subset%>%
   dplyr::select(forecast_target,obs_pr_m_day,obs_tmp_k, fcst_cell)%>%
@@ -100,57 +101,16 @@ hindcast_subset_temp_lags <- hindcast_subset
 for (i in (1:12)){
   mylag <- paste0("lag",i)
   hindcast_subset_temp_lags <- left_join(hindcast_subset_temp_lags, lag.data, by = c("fcst_cell", 
-                                                          setNames("date", mylag)))
+                                                                                     setNames("date", mylag)))
   hindcast_subset_temp_lags[mylag] <- hindcast_subset_temp_lags[,26] #grab observed temp column
   hindcast_subset_temp_lags <- hindcast_subset_temp_lags[,1:24]
 }
 
 #save!
-saveRDS(hindcast_subset_temp_lags, file = "C:\\Users\\ahegedus\\iScience_Project\\data")
+saveRDS(hindcast_subset_temp_lags, file = "C:\\Users\\ahegedus\\Documents\\iScience_Project\\data\\hindcast_subset_temp_lags.rds")
 
-##################################################################
-#create first lag 
-hindcast_all <- hindcast_all %>%
-  mutate(lag1 = substring(forecast_timestamp, 1, 6))
-
-print("Ordering by Forecast Timestamp...")
-#temporal ordering by forecast timestamp (lag1 for more efficiency)
-hindcast_all <- hindcast_all[order(hindcast_all$forecast_timestamp),]
-
-print("Making Dictionary...")
-#make dictionary from %Y%m to temperature (for efficiency)
-map_tt <- NULL
-map_tt <- dict(
-  "sample" = "test", #do this too keep dplyr from breaking Dict library :(
-  .class = "any",
-  .overwrite = FALSE
-)
-
-print("Generating unique mappings...")
-#generate mappings
-for (x in unique(hindcast_all$lag1)) {
-  map_tt[x] <- hindcast_all[match(x, hindcast_all$forecast_target, nomatch=-1),]$obs_tmp_k
-}
-
-#?!?!!?!?!?!?!!?
-print("Generating lags...")
-#make the lags with dplyr. Turns out forecast_timestamp was useful after all!
-hindcast_all <- hindcast_all %>%
-  mutate(lag1 = map_tt[lag1],
-         #for each extra lag, we need to shift back 1 month.
-         #monthly, there are unique 14056 cells, 24 forecasts, and 9 lead times,
-         #so each lag needs to be 14056 * 24 * 9 = 3036096 rows
-         lag2 = lag(lag1, 3036096 * 1),
-         lag3 = lag(lag1, 3036096 * 2),
-         lag4 = lag(lag1, 3036096 * 3),
-         lag5 = lag(lag1, 3036096 * 4),
-         lag6 = lag(lag1, 3036096 * 5),
-         lag7 = lag(lag1, 3036096 * 6),
-         lag8 = lag(lag1, 3036096 * 7),
-         lag9 = lag(lag1, 3036096 * 8),
-         lag10 = lag(lag1, 3036096 * 9),
-         lag11 = lag(lag1, 3036096 * 10),
-         lag12 = lag(lag1, 3036096 * 11))
+#read in above saved file so we don't have to run it again:
+hindcast_subset_temp_lags <- readRDS(file = "C:\\Users\\ahegedus\\Documents\\iScience_Project\\data\\hindcast_subset_temp_lags.rds")
 
 elev_cells <- NULL
 #check for elevation data and generate if it doesn't exist
@@ -168,35 +128,42 @@ if (!file.exists("./data/elev_cells.rds")) {
     is_cell <- (elev_df$x == cells$x[i]) & (elev_df$y == cells$y[i])
     elev_cells[i] <- elev_df$gmted2010_ERA5_quarter_degree[is_cell]
   }
-
-  saveRDS(elev_cells, file = "C:\\Users\\tgause\\iScience_Project\\data\\elev_cells.rds")
+  
+  saveRDS(elev_cells, file = "C:\\Users\\ahegedus\\Documents\\iScience_Project\\data\\elev_cells.rds")
 } else {
-  elev_cells <- readRDS("C:\\Users\\tgause\\iScience_Project\\data\\elev_cells.rds")
+  elev_cells <- readRDS("C:\\Users\\ahegedus\\Documents\\iScience_Project\\data\\elev_cells.rds")
 }
+
+#make elev_cells into a data frame
+elev_df <- data.frame(fcst_cell = sample_data$fcst_cell, x = sample_data$x, 
+                      y = sample_data$y, elevation = elev_cells)
+
+#sanity check: looks right!
+# elev_df%>%
+#   ggplot(mapping = aes(x = x, y = y, color = elevation))+
+#   geom_point()
+
+#remove x,y in elevation data after we graph to make joining easier
+elev_df2 <- elev_df%>%
+  dplyr::select(-c(x,y))
 
 print("Appending elevation data...")
 #now we append our new column...
-hindcast_all <- hindcast_all %>%
-  mutate(elev = c(rep(elev_cells, nrow(hindcast_all)/14056)))
+hindcast_subset_temp_lags <- left_join(hindcast_subset_temp_lags, elev_df2, 
+                                       by = c("fcst_cell"))
 
-#sanity check
-# group_by(x, y) %>%
-# summarize(Elev = mean(elev)) %>%
-# ggplot() +
-# geom_point(aes(x = x,
-#                y = y,
-#                color = Elev))
+#why is this giving me an error? "cannot allocate vector of size 235 Gb"
 
 print("Converting forecast_target to Date...")
 #convert forecast_target to Date object with day=1 for downstream tasks.
-#no need to convert forecast_timestamp (plus it's a nightmare...)
-hindcast_all <- hindcast_all %>%
-  mutate(forecast_target = as.Date(paste0(forecast_target, "01"),
-                                      format = "%Y%m%d",
-                                      tz = "EST"))
+hindcast_subset_temp_lags <- hindcast_subset_temp_lags %>%
+  mutate(forecast_target = as.Date(paste0(hindcast_subset_temp_lags, "01"),
+                                   format = "%Y%m%d",
+                                   tz = "EST"))
 
 print("Saving...")
 #and save!
-saveRDS(hindcast_all, file = "C:\\Users\\tgause\\iScience_Project\\data")
+saveRDS(hindcast_subset_temp_lags, file = "C:\\Users\\ahegedus\\Documents\\iScience_Project\\data\\hindcast_subset_temp_lags_elev.rds")
+
 
 
