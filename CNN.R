3f3883482f0ea6a6f25e420b3a4076a834f17fc3
Katelyn Mei <hillwing115@gmail.com>
2022-05-02 09:08:27 -0400

    update

--- CNN.R
+++ CNN.R
@@ -3,10 +3,12 @@ install.packages("tfruns")
 
 library(keras)
 library(tfruns)
+library(tensorflow)
 
 ####insert training /test data 
-train_data <- NULL
-train_targets <- NULL
+train_data <- sample.x.train
+train_targets <- sample.y.train[,,,5]
+
 ####insert validation data 
 val_data <- NULL
 val_targets <- NULL
@@ -21,50 +23,55 @@ val_targets <- NULL
 ##Build the model 
 # we need to write a function because we need to run the model multiple times 
 build_model <- function() {
-  
-  #build the model
-  model <- keras_model_sequential() %>%
-    layer_conv_2d(filters = 32,
-                  kernel_size = c(3,3),
-                  dilation_rate = 2,
-                  activation = "relu") %>%
-    layer_max_pooling_2d(pool_size = c(2,2)) %>% 
-    layer_conv_2d(filters = 64,
+   model <- keras_model_sequential() %>%
+     layer_conv_2d(filters = 32,
+                   kernel_size = c(3,3),
+                   dilation_rate = 2,
+                   activation = "relu") %>%
+     layer_max_pooling_2d(pool_size = c(2,2)) %>%
+     layer_conv_2d(filters = 64,
+                   kernel_size = c(3,3),
+                   dilation_rate = 1) %>%
+     layer_max_pooling_2d(pool_size = c(2,2)) %>%
+     layer_conv_2d(filters = 128,
+                   kernel_size = c(3,3),
+                   dilation_rate = 1) %>%
+     layer_upsampling_2d(size = c(2,2)) %>%
+     layer_conv_2d(filters = 64,
                   kernel_size = c(3,3),
                   dilation_rate = 1) %>%
-    layer_max_pooling_2d(pool_size = c(2,2)) %>% 
-    layer_conv_2d(filters = 128,
+     layer_upsampling_2d(size = c(2,2)) %>%
+     layer_conv_2d(filters = 32,
                   kernel_size = c(3,3),
-                  dilation_rate = 1) %>%
-    layer_upsampling_2d(size = c(2,2)) %>% 
-    ayer_conv_2d(filters = 64,
-                 kernel_size = c(3,3),
-                 dilation_rate = 1) %>%
-    layer_upsampling_2d(size = c(2,2)) %>% 
-    layer_dense(units = 64, activation = "relu") %>%
-    layer_dense(units = 1) #a single unit and no activation (it will be a linear layer) is 
-                            #a typical setup for scalar regression (a regression where you're 
+                  dilation_rate = 2) %>%
+     layer_dense(units = 64, activation = "relu") %>%
+     layer_dense(units = 1) #a single unit and no activation (it will be a linear layer) is
+                            #a typical setup for scalar regression (a regression where you're
                             #trying to predict a single continuous value).
+
   
-  
-  #select optimization,loss functions 
-  model %>% compile(
-    optimizer = "adam",
-    loss = "mse", # "mse" a widely used loss function for regression problems.
-    metrics = c("mae") #mean absolute error: absolute value of the difference between the predictions and the targets
-  )
-}
+#select optimization,loss functions 
+   model %>% compile(
+     optimizer = "adam",
+     loss = "mse", # "mse" a widely used loss function for regression problems.
+     metrics = c("mae") #mean absolute error: absolute value of the difference between the predictions and the targets
+   )
+ }
 
 ###Train the model 
 
-num_epochs <- 200
+num_epochs <- 10
 all_mae_histories <-NULL
 
+build_model()
 
 history <- model %>% fit(
-    train_data, train_targets,
-    validation_data = list(val_data, val_targets),
-    epochs = num_epochs, batch_size = 1, verbose = 0
+    train_data, 
+    train_targets,
+    validation_data = list(train_data, train_targets),
+    epochs = num_epochs, 
+    batch_size = 1, 
+    verbose = 0
   )
   mae_history <- history$metrics$val_mean_absolute_error
   all_mae_histories <- rbind(all_mae_histories, mae_history)
