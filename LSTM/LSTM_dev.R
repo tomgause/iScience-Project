e89c2d97b37c1783bfe959eebf17e3c62301714e
Gause <tgause@ada.cm.cluster>
2022-05-12 00:12:21 -0400

    a

--- LSTM/LSTM_dev.R
+++ LSTM/LSTM_dev.R
@@ -29,14 +29,17 @@ library(yardstick)
 library(keras)
 library(tensorflow)
 
+setwd("/storage/tgause/iScience_tom/iScience_Project/LSTM")
+
 # Make all the data
 #train <- readRDS("/Users/tomgause/Desktop/iScience_tom/iScience_Project/data/VT_train.RDS")
 #test <- readRDS("/Users/tomgause/Desktop/iScience_tom/iScience_Project/data/VT_test.RDS")
 #train <- readRDS("C:/Users/tgause/iScience_Project/data/train_subset_Vermont_2022-04-16_14-55-49.RDS")
 train <- readRDS("/storage/tgause/iScience_tom/iScience_Project/data/VT_train.RDS")
+test <- readRDS("/storage/tgause/iScience_tom/iScience_Project/data/VT_test.RDS")
 
 train <- train[order(train$forecast_target),]
-#test <- test[order(test$forecast_target, lead, fcst_cell),]
+test <- test[order(test$forecast_target),]
 gc()
 
 train.all.predictions <- train %>%
@@ -48,33 +51,33 @@ train.all.predictions <- train %>%
   dplyr::ungroup() %>%
   select(c(value, index, fcst_cell, lead)) %>%
   as_tbl_time(index = index, lead = lead, fcst_cell = fcst_cell)
-train.months <- unique(train.all.predictions$index)[1:320] # 335 before filter
+train.months <- length(unique(train.all.predictions$index))# 335 before filter
 
 # We'll be using a batch size of 32, so let's reduce this to 320.
-train.all.predictions <- train.all.predictions %>%
-  filter(index %in% train.months)
+#train.all.predictions <- train.all.predictions %>%
+#  filter(index %in% train.months)
 
-#test.all.predictions <- test %>%
-#  select(c(fcst_tmp_k, obs_tmp_k, forecast_target, lead, fcst_cell)) %>%
-#  mutate(value = fcst_tmp_k - obs_tmp_k,
-#         index = as_date(forecast_target)) %>%
-#  dplyr::group_by(forecast_target, fcst_cell, lead) %>%
-#  mutate(value = mean(value)) %>%
-#  dplyr::ungroup() %>%
-#  select(c(value, index, fcst_cell, lead)) %>%
-#  as_tbl_time(index = index, lead = lead, fcst_cell = fcst_cell)
-#test.months <- length(unique(test.all.predictions$index))
+test.all.predictions <- test %>%
+  select(c(fcst_tmp_k, obs_tmp_k, forecast_target, lead, fcst_cell)) %>%
+  mutate(value = fcst_tmp_k - obs_tmp_k,
+         index = as_date(forecast_target)) %>%
+  dplyr::group_by(forecast_target, fcst_cell, lead) %>%
+  mutate(value = mean(value)) %>%
+  dplyr::ungroup() %>%
+  select(c(value, index, fcst_cell, lead)) %>%
+  as_tbl_time(index = index, lead = lead, fcst_cell = fcst_cell)
+test.months <- length(unique(test.all.predictions$index))
 
 # We have 24x duplicate data for each lead-cell-index.
 # By taking the unique data only, we fix this issue.
 train <- train.all.predictions %>% unique()
-#test <- test.all.predictions %>% unique()
+test <- test.all.predictions %>% unique()
 
 rm(train.all.predictions)#, test.all.predictions)
 gc()
 
 train %>% head(2)
-#test %>% head(2)
+test %>% head(2)
 
 # ############################################################################
 # ############################################################################
@@ -209,30 +212,42 @@ train %>% head(2)
 ############################################################################
 # PREPROCESSING
 # LSTM performs better if we center and scale our data.
+# We normalize from 0 to 1.
 
-max <- max(train$value)
-min <- min(train$value)
-
-data.scaled <- train %>%
-  mutate(value = (value - min)/(max - min))
-
-# We only use the first 240 time steps as training data
-train.scaled <- data.scaled %>%
-  dplyr::filter(index %in% train.months[1:240])
+max.train <- max(train$value)
+min.train <- min(train$value)
+train.scaled <- train %>%
+  mutate(value = (value - min.train)/(max.train - min.train))
+max.test <- max(test$value)
+min.test <- min(test$value)
+test.scaled <- test %>%
+  mutate(value = (value - min.test)/(max.test - min.test))
 
 # This is necessary for the agent-based minibatching
 unique.cells <- unique(train$fcst_cell)
 
-# We're going to subset all of the data into data.matrix. This will make
+# We're going to subset all of the data into train.matrix. This will make
 # Minibatching MUCH faster!
-data.matrix <- array(list(), dim=c(9, 51))
+train.matrix <- array(list(), dim=c(9, 51))
 
 # We'll toss the date row--the data's already ordered!
 for (i in c(1:9)) {
   for (j in c(1:length(unique.cells))) {
-    data.matrix[[i, j]] <- as.matrix(data.scaled %>%
+    train.matrix[[i, j]] <- as.matrix(train.scaled %>%
       dplyr::filter(lead == i, fcst_cell == unique.cells[j]))[,-2]
-    class(data.matrix[[i,j]]) <- "numeric"
+    class(train.matrix[[i,j]]) <- "numeric"
+  }
+}
+
+# Same for test data...
+test.matrix <- array(list(), dim=c(9, 51))
+
+# We'll toss the date row--the data's already ordered!
+for (i in c(1:9)) {
+  for (j in c(1:length(unique.cells))) {
+    test.matrix[[i, j]] <- as.matrix(test.scaled %>%
+      dplyr::filter(lead == i, fcst_cell == unique.cells[j]))[,-2]
+    class(test.matrix[[i,j]]) <- "numeric"
   }
 }
 
@@ -275,7 +290,7 @@ generator <- function(data, lookback, delay, min.index, max.index,
       for (CS in cell.sample) {
 
         # grab relevant lead&cell combo
-        temp <- data.matrix[[LS,CS]]
+        temp <- train.matrix[[LS,CS]]
         
         if(shuffle) {
           rows <- sample(c((min.index + lookback):max.index), size = batch.size)
@@ -319,42 +334,43 @@ lead.sample <- 4 # Select 4 lead times
 #   Test: Next 60 timesteps
 #   Validator: Remaining timesteps
 
+
 train.gen = generator(
-  data.matrix,
+  train.matrix,
   lookback = lookback,
   delay = delay,
   min.index = 1,
-  max.index = 200,
+  max.index = 240,
   shuffle = TRUE,
   step = step, 
   batch.size = batch.size
 )
 
 val.gen = generator(
-  data.matrix,
+  train.matrix,
   lookback = lookback,
   delay = delay,
-  min.index = 201,
-  max.index = 260,
+  min.index = 241,
+  max.index = 300,
   step = step,
   batch.size = batch.size
 )
 
 test.gen = generator(
-  data.matrix,
+  test.matrix,
   lookback = lookback,
   delay = delay,
-  min.index = 261,
-  max.index = NULL,
+  min.index = 1,
+  max.index = 150,
   step = step,
   batch.size = batch.size
 )
 
 # How many steps to draw from val.gen in order to see the entire validation set
-val.steps <- floor((260 - 201 - lookback) / batch.size)
+val.steps <- floor((300 - 241 - lookback) / batch.size)
 
 # How many steps to draw from test.gen in order to see the entire test set
-test.steps <- floor((320 - 261 - lookback) / batch.size)
+test.steps <- floor((150 - 1 - lookback) / batch.size)
 
 
 
@@ -377,9 +393,10 @@ evaluate.naive.method <- function() {
 evaluate.naive.method()
 
 
+
+
 ################################################################################
 # Let's try a GRU model!
-
 
 model <- keras_model_sequential() %>% 
   layer_gru(units = 32, input_shape = list(NULL, 3)) %>% 
@@ -390,13 +407,99 @@ model %>% compile(
   loss = "mae"
 )
 
-history <- model %>% fit (
+model %>% fit (
   train.gen,
-  steps_per_epoch = 500,
+  verbose=2,
+  steps_per_epoch = 241 - lookback,
   epochs = 20,
   validation_data = val.gen,
   validation_steps = val.steps
 )
 
+# Let's save the weights!
+model %>% save_model_tf("model")
+model %>% save_model_weights_tf("checkpoints/cp.ckpt")
 
-f
+
+
+################################################################################
+# Add a dropout
+
+model2 <- keras_model_sequential() %>% 
+  layer_gru(units = 32, dropout = 0.2, recurrent_dropout = 0.2,
+            input_shape = list(NULL, 3)) %>% 
+  layer_dense(units = 1)
+
+model2 %>% compile(
+  optimizer = optimizer_rmsprop(),
+  loss = "mae"
+)
+
+model2 %>% fit(
+  train.gen,
+  verbose=2,
+  steps_per_epoch = 241 - lookback,
+  epochs = 40,
+  validation_data = val.gen,
+  validation_steps = val.steps
+)
+
+# Let's save the weights!
+model %>% save_model_tf("model2")
+model %>% save_model_weights_tf("checkpoints/cp2.ckpt")
+
+
+
+################################################################################
+# Next, let's try stacking layers
+
+model3 <- keras_model_sequential() %>% 
+  layer_gru(units = 32, 
+            dropout = 0.1, 
+            recurrent_dropout = 0.5,
+            return_sequences = TRUE,
+            input_shape = list(NULL, 3)) %>% 
+  layer_gru(units = 64, activation = "relu",
+            dropout = 0.1,
+            recurrent_dropout = 0.5) %>% 
+  layer_dense(units = 1)
+
+model3 %>% compile(
+  optimizer = optimizer_rmsprop(),
+  loss = "mae"
+)
+
+model3 %>% fit(
+  train.gen,
+  verbose=2,
+  steps_per_epoch = 241 - lookback,
+  epochs = 40,
+  validation_data = val.gen,
+  validation_steps = val.steps
+)
+
+# Let's save the weights!
+model2 %>% save_model_tf("model3")
+model2 %>% save_model_weights_tf("checkpoints/cp3.ckpt")
+
+# Get a brief summary of the 3 architectures...
+summary(model)
+summary(model2)
+summary(model3)
+
+# and EVALUATE!!
+
+model %>% evaluate(
+    test.gen,
+    verbose=0
+)
+model2 %>% evaluate(
+    test.gen,
+    verbose=0
+)
+model3 %>% evaluate(
+    test.gen,
+    verbose=0
+)
+
+
